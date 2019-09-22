3c6ebd6a9e942d3fec8670c0cbc08ffb2066e98d
Chikashi Miyama <chikashimiyama@gmail.com>
2019-09-22 23:07:11 +0200

    close #8

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow () {
     win = new BrowserWindow({
         width: 800,
         height: 600,
+        resizable: false,
         webPreferences: {
             nodeIntegration: true
         }
